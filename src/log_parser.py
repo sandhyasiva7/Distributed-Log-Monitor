15f3630de922480772decc9f86a146ae5f664792
Sandhya Nagarajan <n.sandhya87@gmail.com>
2025-09-05 15:24:33 -0700

    Adding the parsing of logs from raw to structured data

--- src/log_parser.py
+++ src/log_parser.py
@@ -1,5 +1,28 @@
 '''
+part 1:
 Reads logs line by line.
-Parses timestamps, severity, service name, message.
+Parses timestamps, severity, service name, message and convert raw loglines 
+to structured data
+part 2:
 Deduplicates repeated errors within a time window.
 '''
+
+from config_loader import load_and_validate_config
+
+def parse_logs():
+    validated_services = load_and_validate_config()
+
+    structured_line = {}
+    structured_log = []
+
+    for service in validated_services:
+        with open(service['log_path']) as log_file:
+            for line in log_file:
+                timestamp,serverity,message = line.split(" ",2)
+                structured_line = {'timestamp': timestamp, 'service':service['name'],
+                                'serverity': serverity,'message': message }
+                structured_log.append(structured_line)                
+    return structured_log
+
+#testing
+print(parse_logs())
