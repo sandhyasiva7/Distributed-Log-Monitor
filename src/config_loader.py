86678faca908a16915b1faece45e441bfa625768
Sandhya Nagarajan <sandhyanagarajan@Sandhyas-MacBook-Air.local>
2025-09-05 14:20:58 -0700

    Improved config_loader to exit when there are issues with the file and made it into method return only valid services

--- src/config_loader.py
+++ src/config_loader.py
@@ -2,42 +2,57 @@
 Loads YAML config into Python objects.
 
 Validates fields (error thresholds, log paths).
+
+Return the services only with valid fields
 '''
 import yaml,logging
 from pathlib import Path
-
-
 # basic setup for logging
 logging.basicConfig(filename='./application_logs/config_loader.log',
                     level=logging.INFO,
                     format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
-try:
-    with open("./config.yaml") as file:
-        data = yaml.safe_load(file)
-except yaml.YAMLError as e:
-    logger.error(f"The yaml is not safe to load, see the error {e} for more information")
-except FileNotFoundError:
-    logger.error(f"{file} doesn't exists")
 
-# validating services, service, log_path and error threshold
-services = data.get('services')
-if services is None:
-    logger.error("services field doesn't exists")
-for service in services:
-    service_name = service.get('name')
-    log_path = service.get('log_path')
-    error_threshold = service.get('error_threshold')
-    if service_name is None:
-        logger.error(f"service_name is None")
-    if type(error_threshold) is not int:
-        logger.error(f"error_threshold = {error_threshold} must be an int for {service_name}")
-    if log_path is None:
-        logger.error(f"log_path is None for {service_name}")
-    elif Path(log_path).exists():
-        logger.info(f"{log_path} exists,safe to proceed for this {service_name}")
-    else:
-        logger.error(f"{log_path} doesn't exists, for this service {service_name}")
+validation_passed_services = []
+
+def load_and_validate_config(file="./config.yaml"):
+    try:
+        with open("./config.yaml") as file:
+            data = yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        logger.error(f"The yaml is not safe to load, see the error {e} for more information")
+        exit
+    except FileNotFoundError:
+        logger.error(f"{file} doesn't exists")
+        exit
+
+    # validating services, service, log_path and error threshold
+    services = data.get('services')
+    if services is None:
+        logger.error("services field doesn't exists")
+    for service in services:
+        service_name = service.get('name')
+        log_path = service.get('log_path')
+        error_threshold = service.get('error_threshold')
+        valid = True
+        if service_name is None:
+            logger.error(f"service_name is None")
+            valid = False
+        if type(error_threshold) is not int:
+            logger.error(f"error_threshold = {error_threshold} must be an int for {service_name}")
+            valid = False
+        if log_path is None:
+            logger.error(f"log_path is None for {service_name}")
+            valid = False
+        if not Path(log_path).exists():
+            logger.info(f"{log_path} doesn't exists, for this service {service_name}")
+            valid = False
+        if valid:
+            validation_passed_services.append(service)
+    return validation_passed_services
+
+# Testing
+#print(load_and_validate_config("./config.yaml"))
 
 
         
